9154bc89941df5163ecbc1554d69a54f5f91d0c8
je-ni <jenniferolibie@gmail.com>
2018-06-08 13:09:47 +0100

    My basic version with JSON is working

--- bookAPItest.js
+++ bookAPItest.js
@@ -1,25 +1,25 @@
 var app = require('express')();
 
-app.listen(3000, function(){
-    console.log('server is listening on port 3000');
-});
+// app.listen(3000, function(){
+//     console.log('server is listening on port 3000');
+// });
 
 var lib = new Library('Lib');
 
-app.get('/', function(request, response){
+app.get('/', function (request, response) {
     response.send(lib.getBooks());
 })
 
 var fs = require('fs');
 
-function Book(title, author, year, id){
+function Book(title, author, year, id) {
     this.title = title;
     this.author = author;
     this.year = year;
     this.id = id;
 }
 
-function Library(name){
+function Library(name) {
     this.name = name;
     this.books = [];
     // this.books = fs.readFileSync('./books.json', 'utf-8');
@@ -30,65 +30,65 @@ function Library(name){
     //     writable: true
     // })
 }
-Library.prototype.getLibrary = function(){
+Library.prototype.getLibrary = function () {
     return JSON.parse(fs.readFileSync('./books.json'));
 }
 
-Library.prototype.updateLibrary = function(){
+Library.prototype.updateLibrary = function () {
     return fs.writeFileSync('./books.json', JSON.stringify(this.books));
 }
 
-Library.prototype.addBook = function(book){
+Library.prototype.addBook = function (book) {
     this.books.push(book);
     this.updateLibrary();
 }
 
-Library.prototype.getBooks = function(){
+Library.prototype.getBooks = function () {
     this.books = this.getLibrary();
-    return this.books; 
+    return this.books;
+}
 
-Library.prototype.getBookById = function(id){
+Library.prototype.getBookById = function (id) {
     // var book = this.books.filter(book => book.id == id);
     this.books = this.getLibrary();
-    for (let i = 0; i < this.books.length; i++){
-        if(this.books[i].id === id){
+    for (let i = 0; i < this.books.length; i++) {
+        if (this.books[i].id === id) {
             return this.books[i];
-            }
-    }
-            return `Book with id: ${id} not found.`;
         }
     }
+    return `Book with id: ${id} not found.`;
+}
 
-Library.prototype.getBookIndex = function(id){
+Library.prototype.getBookIndex = function (id) {
     this.books = this.getLibrary();
-    for (let i = 0; i < this.books.length; i++){
-        if(this.books[i].id === id){
+    for (let i = 0; i < this.books.length; i++) {
+        if (this.books[i].id === id) {
             return i;
-            }
-    }
-            return `Book with id: ${id} not found.`;
         }
+    }
+    return `Book with id: ${id} not found.`;
+}
 
-Library.prototype.deleteBook = function(id){
+Library.prototype.deleteBook = function (id) {
     let bookIndex = this.getBookIndex(id);
     var message = "You just deleted the book, '" + this.books[bookIndex].title +
-                  " by " + this.books[bookIndex].author +"("+this.books[bookIndex].year + ")'."; 
+        " by " + this.books[bookIndex].author + "(" + this.books[bookIndex].year + ")'.";
     this.books.splice(bookIndex, 1);
     this.updateLibrary(this.books);
     return message;
 }
 
-Library.prototype.updateBook = function(id, updatedBook){
+Library.prototype.updateBook = function (id, updatedBook) {
     let bookIndex = this.getBookIndex(id);
     this.books[bookIndex] = updatedBook;
     this.updateLibrary(this.books);
 }
 
-Library.prototype.getBooksByParam = function(param, value){
+Library.prototype.getBooksByParam = function (param, value) {
     this.books = this.getLibrary();
     var books = [];
-    for (let i = 0; i < this.books.length; i++){
-        if(this.books[i][param] === value){
+    for (let i = 0; i < this.books.length; i++) {
+        if (this.books[i][param] === value) {
             books.push(this.books[i]);
         }
     }
@@ -110,5 +110,3 @@ lib.addBook(book3);
 // console.log(lib.getBooksByParam('year', 2018));
 // console.log(lib.deleteBook(1));
 // console.log(lib.books);
-
-
